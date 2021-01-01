ee9038c00cbbe6c91d2c71cbd1186497ec51bd0b
unknown <zlovred12@gmail.com>
2021-01-01 20:38:38 +0300

    codewars

---
+++ src/main/java/WhitespaceNumber.java
@@ -0,0 +1,51 @@
+public class WhitespaceNumber {
+
+    public static String whitespaceNumber(final int n) {
+        String temp = String.valueOf(n);
+        StringBuilder sb = new StringBuilder();
+        String binary = "";
+        int convert = 0;
+
+        for (int i = 0; i < temp.length() ; i++) {
+            if (temp.charAt(i) == '-') {
+                sb.append("\\t");
+            }else{
+                sb.append(" ");
+            }
+        }
+        if (temp.charAt(0) == '-'){
+            temp = temp.substring(1);
+            convert = Integer.valueOf(temp);
+        }
+
+        binary = intToBinary(convert);
+
+        for (int i = 0; i < binary.length() ; i++) {
+            if (binary.charAt(i) == '1'){
+                sb.append("\\t");
+            }else  if (binary.charAt(i) == '0'){
+                sb.append(" ");
+            }
+        }
+
+        sb.append("\\n");
+
+        return sb.toString();
+    }
+
+    private static String intToBinary(int n){
+        String result = "";
+
+        while (n > 0){
+            if (n % 2 == 0){
+                result += "0";
+            }else  if (n % 2 != 0){
+                result += "1";
+            }
+
+            n = n / 2;
+        }
+
+       return new StringBuilder(result).reverse().toString();
+    }
+}
