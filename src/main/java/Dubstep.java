550178db53cd5e7dbf5e6c57ce5eeff5ee22a79a
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-03 12:50:38 +0300

    Dubstep

--- src/main/java/Dubstep.java
+++ src/main/java/Dubstep.java
@@ -1,42 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Dubstep {
-    public static String SongDecoder(String song) {
-        String temp = song.replace("WUB", "1");
-        String[] arr = temp.split("");
+
+    public static String SongDecoder (String song) {
+        String temp = song.replaceAll("WUB", "1");
+        String[] arr = temp.split("1");
         StringBuilder sb = new StringBuilder();
-        String first = "";
-        String second = "";
-        HashMap<Integer, String> map = new HashMap<>();
-        if (!arr[0].equals("1")) {
-            first = arr[0];
-        }
-        if (!arr[arr.length - 1].equals("1")) {
-            second = arr[arr.length - 1];
-        }
-
-        for (int i = 1; i < arr.length - 1; i++) {
-            map.put(i, arr[i]);
-        }
-
-        if (!first.equals("")) {
-            sb.append(first);
-        }
         String s = "";
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            if (!entry.getValue().equals("1") && entry.getKey() != 2) {
-                sb.append("" + entry.getValue());
-            } else if (!entry.getValue().equals("1")) {
-                sb.append(entry.getValue());
-            }
-            if (entry.getKey().equals("1") && entry.getValue().equals("1")){
-                System.out.println(1);
+
+        for (int i = 0; i < arr.length ; i++) {
+            if (!arr[i].equals(" ")){
+                sb.append(arr[i] + " ");
             }
         }
 
-        System.out.println(s);
+        s = sb.toString();
+        s = s.trim().replaceAll(" +", " ");
 
-        return "s";
+
+        return s;
     }
 }
-
