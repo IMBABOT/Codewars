2b56e6edeffa555de6f133471effbf4c1b830aff
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-11 07:43:39 +0300

    temp

--- src/main/java/ExpandedForm.java
+++ src/main/java/ExpandedForm.java
@@ -42,7 +42,6 @@ public class ExpandedForm {
                     j++;
                 }else if (times > 1){
                     j = i + 1;
-
             }else if (arr[j] == 0){
                 j++;
             }
