065c998d4ade9fe9fcdf98f07d9b496bd5df7e4e
IMBABOT1 <zlovred12@gmail.com>
2021-02-02 23:39:45 +0300

    iterator

--- src/main/java/DirReduc.java
+++ src/main/java/DirReduc.java
@@ -1,43 +1,49 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class DirReduc {
 
-    public static String[] dirReduc(String[] arr) {
-        String n = "NORTH";
-        String s = "SOUTH";
-        String e = "EAST";
-        String w = "WEST";
-        int index = 0;
-        do {
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i].equals(n) && arr[i + 1].equals(s)) {
-                    arr[i] = "";
-                    arr[i + 1] = "";
-                }
-                if (arr[i].equals(s) && arr[i + 1].equals(n)) {
-                    arr[i] = "";
-                    arr[i + 1] = "";
-                }
-                if (arr[i].equals(w) && arr[i + 1].equals(e)) {
-                    arr[i] = "";
-                    arr[i + 1] = "";
-                }
-                if (arr[i].equals(e) && arr[i + 1].equals(w)) {
-                    arr[i] = "";
-                    arr[i + 1] = "";
-                }
-                index++;
-            }
-        }    while (arr[index].equals(n) && arr[index + 1].equals(w) ||
-                arr[index].equals(w) && arr[index + 1].equals(n) ||
-                arr[index].equals(n) && arr[index + 1].equals(e) ||
-                arr[index].equals(e) && arr[index + 1].equals(n) ||
-                arr[index].equals(s) && arr[index + 1].equals(e) ||
-                arr[index].equals(e) && arr[index + 1].equals(s) ||
-                arr[index].equals(s) && arr[index + 1].equals(w) ||
-                arr[index].equals(w) && arr[index + 1].equals(s)) ;
+        public static String[] dirReduc(String[] arr) {
+            String n = "NORTH";
+            String s = "SOUTH";
+            String e = "EAST";
+            String w = "WEST";
+            List<String> strings = new ArrayList<>(Arrays.asList(arr));
+            int index = 0;
 
-        return new String[]{};
+            if (index < arr.length - 1) {
+                for (int i = 0; i < strings.size() - 1; i++) {
+                    if (strings.get(i).equals(n) && strings.get(i + 1).equals(s)) {
+                        strings.set(i, "");
+                        strings.set(i + 1, "");
+                    }
+                    if (strings.get(i).equals(s) && strings.get(i + 1).equals(n)) {
+                        strings.set(i, "");
+                        strings.set(i + 1, "");
+                    }
+                    if (strings.get(i).equals(w) && strings.get(i + 1).equals(e)) {
+                        strings.set(i, "");
+                        strings.set(i + 1, "");
+                    }
+                    if (strings.get(i).equals(e) && strings.get(i + 1).equals(w)) {
+                        strings.set(i, "");
+                        strings.set(i + 1, "");
+                    }
+                }
+                Iterator<String> iterator = strings.iterator();
+                while (iterator.hasNext()) {
+                    String o = iterator.next();
+                    if (o.equals("")) {
+                        iterator.remove();
+                    }
+                }
+
+
+            }
+            return new String[]{};
+        }
     }
-}
+
 
