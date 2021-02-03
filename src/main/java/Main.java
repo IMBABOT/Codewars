1f29a990b1875a0103d9b2b7586276e29769726c
IMBABOT1 <zlovred12@gmail.com>
2021-02-03 12:22:58 +0300

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(DirReduc.dirReduc(new String[]{"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"})));
+        WhereIsMyParent.findChildren("abBA");
     }
 }
---
+++ src/main/java/WhereIsMyParent.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class WhereIsMyParent {
+    public static String findChildren(final String text) {
+        char[] word=text.toCharArray();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i <word.length - 1 ; i++) {
+            for (int j = 0; j <word.length - i -1 ; j++) {
+                    if (word[j] > word[j + 1]){
+                        char temp = word[j];
+                        word[j] = word[j + 1];
+                        word[j + 1] = temp;
+                    }
+                }
+            }
+
+        for (int i = 0; i < word.length ; i++) {
+            list.add(String.valueOf(word[i]));
+        }
+        Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+
+
+        System.out.println(list);
+
+        return "";
+    }
+}
+
