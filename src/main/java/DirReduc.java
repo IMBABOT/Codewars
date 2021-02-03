d8981f01a2976bdf84f8e2d1af1b4ca9f119a672
IMBABOT1 <zlovred12@gmail.com>
2021-02-03 11:56:35 +0300

    temp

--- src/main/java/DirReduc.java
+++ src/main/java/DirReduc.java
@@ -5,45 +5,61 @@ import java.util.List;
 
 public class DirReduc {
 
-        public static String[] dirReduc(String[] arr) {
-            String n = "NORTH";
-            String s = "SOUTH";
-            String e = "EAST";
-            String w = "WEST";
-            List<String> strings = new ArrayList<>(Arrays.asList(arr));
-            int index = 0;
+    public static String[] dirReduc(String[] arr) {
+        String n = "NORTH";
+        String s = "SOUTH";
+        String e = "EAST";
+        String w = "WEST";
+        List<String> strings = new ArrayList<>(Arrays.asList(arr));
+        int index = 0;
 
-            if (index < arr.length - 1) {
-                for (int i = 0; i < strings.size() - 1; i++) {
-                    if (strings.get(i).equals(n) && strings.get(i + 1).equals(s)) {
-                        strings.set(i, "");
-                        strings.set(i + 1, "");
-                    }
-                    if (strings.get(i).equals(s) && strings.get(i + 1).equals(n)) {
-                        strings.set(i, "");
-                        strings.set(i + 1, "");
-                    }
-                    if (strings.get(i).equals(w) && strings.get(i + 1).equals(e)) {
-                        strings.set(i, "");
-                        strings.set(i + 1, "");
-                    }
-                    if (strings.get(i).equals(e) && strings.get(i + 1).equals(w)) {
-                        strings.set(i, "");
-                        strings.set(i + 1, "");
+        for (int i = 0; i < strings.size(); i++) {
+            for (int j = 0; j < strings.size() - 1; j++) {
+                if (strings.get(j).equals(n) && strings.get(j + 1).equals(e) ||
+                        strings.get(j).equals(e) && strings.get(j + 1).equals(n) ||
+                        strings.get(j).equals(n) && strings.get(j + 1).equals(w) ||
+                        strings.get(j).equals(w) && strings.get(j + 1).equals(n) ||
+                        strings.get(j).equals(s) && strings.get(j + 1).equals(e) ||
+                        strings.get(j).equals(e) && strings.get(j + 1).equals(s) ||
+                        strings.get(j).equals(s) && strings.get(j + 1).equals(w) ||
+                        strings.get(j).equals(w) && strings.get(j + 1).equals(s)) {
+
+                        for (int h = 0; h < strings.size() - 1; h++) {
+                            if (strings.get(h).equals(n) && strings.get(h + 1).equals(s)) {
+                                strings.set(h, "");
+                                strings.set(h + 1, "");
+                            }
+                            if (strings.get(h).equals(s) && strings.get(h + 1).equals(n)) {
+                                strings.set(h, "");
+                                strings.set(h + 1, "");
+                            }
+                            if (strings.get(h).equals(w) && strings.get(h + 1).equals(e)) {
+                                strings.set(h, "");
+                                strings.set(h + 1, "");
+                            }
+                            if (strings.get(h).equals(e) && strings.get(h + 1).equals(w)) {
+                                strings.set(h, "");
+                                strings.set(h + 1, "");
+                            }
+                        }
+
+                        Iterator<String> iterator = strings.iterator();
+                        while (iterator.hasNext()) {
+                            String o = iterator.next();
+                            if (o.equals("")) {
+                                iterator.remove();
+                            }
+                        }
                     }
                 }
-                Iterator<String> iterator = strings.iterator();
-                while (iterator.hasNext()) {
-                    String o = iterator.next();
-                    if (o.equals("")) {
-                        iterator.remove();
-                    }
-                }
-
-
             }
-            return new String[]{};
-        }
+
+        String[] result = strings.toArray(new String[strings.size()]);
+        return result;
     }
+}
+
+
+
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        DirReduc.dirReduc(new String[]{"NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"});
+        System.out.println(Arrays.toString(DirReduc.dirReduc(new String[]{"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"})));
     }
 }
