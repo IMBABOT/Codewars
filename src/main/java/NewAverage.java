df1b16ab0e28a8e139b69179e41b341cc6e11267
IMBABOT1 <zlovred12@gmail.com>
2021-02-04 12:20:37 +0300

    NewAverage

--- src/main/java/NewAverage.java
+++ src/main/java/NewAverage.java
@@ -3,59 +3,19 @@ import java.util.Arrays;
 public class NewAverage {
 
     public static long newAvg(double[] arr, double navg) {
-        double result = 0;
-        double avr = 0;
-        double dot = 0;
-        double dot1 = 0;
-        long res = 0;
+        long result = 0;
+        double sum = 0;
+
 
         for (int i = 0; i < arr.length; i++) {
-            avr += arr[i];
+            sum += arr[i];
         }
 
+        result = (long)Math.ceil((navg * (arr.length + 1) - (sum)));
 
-
-        dot = (navg % 1);
-        dot1 = (avr % 1);
-
-
-        if (dot > 0 || dot1 > 0){
-            result = (long) (navg * (arr.length + 1) - (avr));
-
-            res = (long) result;
-            res = res + 1;
-
-            System.out.println(avr);
-            System.out.println(avr);
-
-            System.out.println(dot);
-            System.out.println(dot);
-
-            if (result <= 0) {
-                throw new IllegalArgumentException();
-            }
-            System.out.println(result);
+        if (result <= 0) {
+            throw new IllegalArgumentException();
         }
 
-        if (dot == 0 && dot1 == 0) {
-            result = (long) (navg * (arr.length + 1) - (avr));
-
-            res = (long) result;
-
-            System.out.println(avr);
-            System.out.println(avr);
-
-
-            System.out.println(dot);
-            System.out.println(dot);
-
-
-            if (result <= 0) {
-                throw new IllegalArgumentException();
-            }
-
-            System.out.println(result);
-        }
-        return res;
     }
 }
