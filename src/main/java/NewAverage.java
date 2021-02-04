3b20c0dd9b55888d824071d516f3da92c45a703d
IMBABOT1 <zlovred12@gmail.com>
2021-02-04 12:17:30 +0300

    newAverage

--- src/main/java/NewAverage.java
+++ src/main/java/NewAverage.java
@@ -3,56 +3,59 @@ import java.util.Arrays;
 public class NewAverage {
 
     public static long newAvg(double[] arr, double navg) {
-        long result = 0;
+        double result = 0;
         double avr = 0;
         double dot = 0;
-
-
+        double dot1 = 0;
+        long res = 0;
 
         for (int i = 0; i < arr.length; i++) {
             avr += arr[i];
         }
 
-        dot = (avr % 1);
 
 
-
-        System.out.println(avr);
-        System.out.println(avr);
-
+        dot = (navg % 1);
+        dot1 = (avr % 1);
 
 
-        System.out.println(dot);
-        System.out.println(dot);
+        if (dot > 0 || dot1 > 0){
+            result = (long) (navg * (arr.length + 1) - (avr));
 
+            res = (long) result;
+            res = res + 1;
 
-        if (dot == 0) {
+            System.out.println(avr);
+            System.out.println(avr);
 
-            result = (long) (navg * (arr.length + 1) - avr);
-
-            if (result <= 0) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        if (dot > 0){
-
-            result = (long) (navg * (arr.length + 1) - (avr - 1));
+            System.out.println(dot);
+            System.out.println(dot);
 
             if (result <= 0) {
                 throw new IllegalArgumentException();
             }
-
+            System.out.println(result);
         }
 
-        System.out.println(result);
-        System.out.println(result);
-        System.out.println(result);
-        System.out.println(result);
-        System.out.println(result);
+        if (dot == 0 && dot1 == 0) {
+            result = (long) (navg * (arr.length + 1) - (avr));
+
+            res = (long) result;
+
+            System.out.println(avr);
+            System.out.println(avr);
 
 
-        return result;
+            System.out.println(dot);
+            System.out.println(dot);
 
+
+            if (result <= 0) {
+                throw new IllegalArgumentException();
+            }
+
+            System.out.println(result);
+        }
+        return res;
     }
 }
